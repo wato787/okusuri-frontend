50328e3264c3581eabf54fa8649278d62e7c3cb1
wato787 <wato787@icloud.com>
2025-04-14 21:03:50 +0900

    fcm

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ_uI-RuoTzt0HE6-8Bdc1lsRpuAiyWRs",
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "okusuri-fcm.firebaseapp.com",
   projectId: "okusuri-fcm",
   storageBucket: "okusuri-fcm.firebasestorage.app",
@@ -19,3 +20,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+let messaging;
+if (typeof window !== 'undefined') {
+  try {
+    messaging = getMessaging(app);
+  } catch (error) {
+    console.error('Firebase messaging error:', error);
+  }
+}
+
+export { app, messaging };
