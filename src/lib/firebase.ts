0e8cfa526ac434087a85f9424a17b036c2a412bf
wato787 <wato787@icloud.com>
2025-04-14 21:36:12 +0900

    add firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,33 +1,33 @@
+import { getAnalytics } from "firebase/analytics";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging } from "firebase/messaging";
+import { type Messaging, getMessaging } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "okusuri-fcm.firebaseapp.com",
-  projectId: "okusuri-fcm",
-  storageBucket: "okusuri-fcm.firebasestorage.app",
-  messagingSenderId: "725863494",
-  appId: "1:725863494:web:f812ecb4d5a8124a338e47",
-  measurementId: "G-QGPNCZTBCL"
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: "okusuri-fcm.firebaseapp.com",
+	projectId: "okusuri-fcm",
+	storageBucket: "okusuri-fcm.firebasestorage.app",
+	messagingSenderId: "725863494",
+	appId: "1:725863494:web:f812ecb4d5a8124a338e47",
+	measurementId: "G-QGPNCZTBCL",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-let messaging;
-if (typeof window !== 'undefined') {
-  try {
-    messaging = getMessaging(app);
-  } catch (error) {
-    console.error('Firebase messaging error:', error);
-  }
+let messaging: Messaging;
+if (typeof window !== "undefined") {
+	try {
+		messaging = getMessaging(app);
+	} catch (error) {
+		console.error("Firebase messaging error:", error);
+	}
 }
 
-export { app, messaging };
+export { app, messaging, analytics };
