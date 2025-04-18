a2e329a48fb690b6adcdcad995186695efc70433
wato787 <wato787@icloud.com>
2025-04-18 19:51:56 +0900

    page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,13 @@
 'use client';
 
+import NotificationSettingContent from './_components/NotificationSettingContent';
+
 const page = () => {
-  // const fcmToken = getToken(messaging, {
-  //   vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-  // })
-  //   .then((currentToken) => {
-  //     if (currentToken) {
-  //       console.log('currentToken', currentToken);
-  //       return currentToken;
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.log('An error occurred while retrieving token. ', err);
-  //   });
-  return <div>page</div>;
+  return (
+    <div>
+      <NotificationSettingContent />
+    </div>
+  );
 };
 
 export default page;
