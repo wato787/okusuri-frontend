22d72619cee1c615d3fde51be1d64551a1b52fd4
wato787 <wato787@icloud.com>
2025-05-04 22:50:16 +0900

    manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,10 +2,10 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
 	return {
-		name: "Next.js PWA",
-		short_name: "NextPWA",
-		description: "A Progressive Web App built with Next.js",
-		start_url: "/",
+		name: "Okusuri",
+		short_name: "Okusuri",
+		description: "おくすり",
+		start_url: "/dashborad",
 		display: "standalone",
 		background_color: "#ffffff",
 		theme_color: "#000000",
