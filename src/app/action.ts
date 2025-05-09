2a35411ddc89466b321d629847a73f94e883df5a
wato787 <wato787@icloud.com>
2025-05-10 00:34:21 +0900

    refactor

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,25 +1,6 @@
 "use server";
 
-import { type BaseResponse, post } from "@/utils/apiBase";
-import type { RegisterNotificationSetting } from "./schema";
-
-export const registerNotificationSetting = async (
-	req: RegisterNotificationSetting,
-) => {
-	const res = await post<RegisterNotificationSetting, BaseResponse>(
-		"/notification/setting",
-		req,
-	);
-
-	if (res.status !== 200) {
-		return {
-			success: false,
-		};
-	}
-	return {
-		success: true,
-	};
-};
+import { post } from "@/utils/apiBase";
 
 export const registerMedicationLog = async (req: {
 	hasBleeding: boolean;
