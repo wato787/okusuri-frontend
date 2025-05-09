8ebc927d1a07014e56b77e07ef0558c23b809f00
wato787 <wato787@icloud.com>
2025-05-10 00:34:24 +0900

    refactor

---
+++ src/app/setting/action.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { type BaseResponse, post } from "@/utils/apiBase";
+import type { RegisterNotificationSetting } from "./schema";
+
+export const registerNotificationSetting = async (
+	req: RegisterNotificationSetting,
+) => {
+	const res = await post<RegisterNotificationSetting, BaseResponse>(
+		"/notification/setting",
+		req,
+	);
+
+	if (res.status !== 200) {
+		return {
+			success: false,
+		};
+	}
+	return {
+		success: true,
+	};
+};
